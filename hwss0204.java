f04425c71176b36d2fd2c1871b11a7e976e38456
quan41144 <quan41144@gmail.com>
2026-03-05 01:56:19 +0700

    Add files via upload

---
+++ hwss0204.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class hwss0204 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if ((a+b<=c || a+c<=b||b+c<=a) || (a <= 0 || b <= 0 || c <= 0)) {
+            System.out.println("Ba cạnh không tạo thành tam giác.");
+        }
+        else {
+            if (a == b && b == c) {
+                System.out.println("Đây là tam giác đều.");
+            }
+            else if (a == b || b == c || c == a) {
+                System.out.println("Đây là tam giác cân.");
+            }
+            else if (a*a+b*b == c*c || a*a+c*c==b*b || b*b+c*c==a*a) {
+                System.out.println("Đây là tam giác vuông.");
+            }
+            else {
+                System.out.println("Đây là tam giác thường.");
+            }
+        }
+    }
+}
